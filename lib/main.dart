74b613b59716fa120aa48ef0443e6e0ba873c34d
Shubham Agrawal <shubhs0707@gmail.com>
2020-10-11 20:14:41 +0530

    places app created with basic boilerplate

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/great_places.dart';
+
+import './screens/places_list_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => GreatPlaces(),
+      child: MaterialApp(
+        title: "Great Places",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+          brightness: Brightness.dark,
+        ),
+        home: PlacesListScreen(),
+      ),
+    );
+  }
+}
